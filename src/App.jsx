f271a06cfda6b58d693d5fefe5ca13c864cbd096
Rinshad TM <rinshadthamerasseri@gmail.com>
2025-03-28 17:26:24 +0530

    committed

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>Welcome</div>
+      <div className="bg-lime-50 text-green-700">
+       <table>
+        <th>Name </th>
+        <th>place</th>
+        <th>phone</th>
+        <tr></tr>
+        <td>Abiram</td>
+        <td>perinthalmanna</td>
+        <td>9045362725</td>
+        <tr></tr>
+        <td>Aflah</td>
+        <td>perinthalmanna</td>
+        <td>9045362725</td>
+        <tr></tr>
+        <td>Nufais</td>
+        <td>perinthalmanna</td>
+        <td>9045362725</td>
+       </table>
+       </div>
+    </>
+  )
+}
+
+export default App
