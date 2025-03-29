e2d0f1be165ff06983c7de608ebd11adeb21d475
Rinshad TM <rinshadthamerasseri@gmail.com>
2025-03-29 09:19:03 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-
+import React from 'react'
 
 function App() {
-
-
   return (
-    <>
-      <div>Welcome</div>
-      <div className="bg-lime-50 text-green-700">
-       <table>
-        <th>Name </th>
-        <th>place</th>
-        <th>phone</th>
-        <tr></tr>
-        <td>Abiram</td>
-        <td>perinthalmanna</td>
-        <td>9045362725</td>
-        <tr></tr>
-        <td>Aflah</td>
-        <td>perinthalmanna</td>
-        <td>9045362725</td>
-        <tr></tr>
-        <td>Nufais</td>
-        <td>perinthalmanna</td>
-        <td>9045362725</td>
-       </table>
-       </div>
-    </>
+    <div>
+      <header>
+  <span>
+    <button>HOME </button>
+    <button>ABOUTE</button>
+    <button>MENU</button>
+    <button>MORE</button>
+  </span>
+ </header>
+    </div>
   )
 }
-
+ 
 export default App
